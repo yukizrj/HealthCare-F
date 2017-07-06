35fd8ddbc8e98465544c9a43e6843312256a9d65
yukizrj <rujiezheng124@gmail.com>
2017-07-06 12:07:57 -0500

    doctor-view -profile

--- app/doctors/doctors-routes.config.ts
+++ app/doctors/doctors-routes.config.ts
@@ -3,7 +3,7 @@ import {RouteInfo} from "./doctors.metadata";
 export const Route: RouteInfo[] = [
 
     { path: './doctor-profile', title: 'Change Password',  icon: 'dashboard', class: '' },
-    { path: './doctor-view-profile', title: 'View Profile',  icon:'content_paste', class: '' },
+    /*{ path: './doctor-view-profile', title: 'View Profile',  icon:'content_paste', class: '' },*/
     { path: './doctor-view-appointment', title: 'Appointments',  icon:'person', class: '' },
     { path: '/', title: 'Logout',  icon:'exit_to_app', class: '' },
     /*{ path: './leave', title: 'Apply Leave',  icon:'content_paste', class: '' },*/
